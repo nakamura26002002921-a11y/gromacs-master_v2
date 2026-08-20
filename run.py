561898757b4c2aef6fcf1142cf6e5e2e90050b29
nakamura26002002921-a11y <nakamura26002002921@gmail.com>
2026-08-20 14:55:41 +0900

    Create run.py

---
+++ run.py
@@ -0,0 +1,14 @@
+# ============================================================
+# Usage:
+#   python3 run.py --cmd "XXX" --path "YYY" -o "ZZZ"
+# ============================================================
+import argparse, subprocess
+
+p = argparse.ArgumentParser()
+p.add_argument("--cmd")
+p.add_argument("--path")
+p.add_argument("-o")
+a = p.parse_args()
+
+subprocess.run(a.cmd, shell=True, cwd=a.path)
+open(a.o, "a").write(a.cmd + "\n")
